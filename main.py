e86f8dd17cdf41ce9bcd9fa883718fcc73fa5b15
Spana21 <115161965+Spana21@users.noreply.github.com>
2023-02-01 21:05:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,18 +7,20 @@ def main():
     number = int(input("Kolik disku? (max 7): "))
     pocet = int(input("Kolik sloupcu?(3 nebo 4): "))
 
-    win = turtle.Screen()
-    run_turtle(win)
 
     Disky(number)
 
     if pocet == 3:
         sloupec = int(input("Na ktery sloupec? (2 nebo 3): "))
+        win = turtle.Screen()
+        run_turtle(win)
         Sloupce3(sloupec,number)
 
 
     if pocet == 4:
         sloupec = int(input("Na ktery sloupec? (2, 3 nebo 4): "))
+        win = turtle.Screen()
+        run_turtle(win)
         Sloupce4(sloupec,number)
 
     turtle.done() #okno se po animace nezavre
